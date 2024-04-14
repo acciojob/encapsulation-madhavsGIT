ab23149802dad101ff159b4ec2abd36bb8206a33
madhavsGIT <kmadhav455@gamil.com>
2024-04-14 16:28:45 +0530

    thrid try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Main {
     public void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        String Name = sc.nextLine();
         RWOnly obj = new RWOnly();
 
-        obj.setName("name");
+        obj.setName(Name);
         System.out.println(obj.getName());
     }
 }

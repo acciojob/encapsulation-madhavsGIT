54be7a20d2ec540e49ce4055432c09057beb08aa
madhavsGIT <kmadhav455@gamil.com>
2024-04-15 05:57:47 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String Name = sc.nextLine();
+
+
         RWOnly obj = new RWOnly();
 
-        obj.setName(Name);
+        obj.setName("Name");
         System.out.println(obj.getName());
     }
 }
